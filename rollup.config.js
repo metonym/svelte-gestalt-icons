f93c91020c7f6d0a67dff364eb9960b63ef245c6
Eric Liu <ericyl.us@gmail.com>
2020-11-30 05:56:53 -0800

    chore: rebuild

--- rollup.config.js
+++ rollup.config.js
@@ -6,4 +6,19 @@ export default svelteReadme({
   minify: !DEV,
   svelte: { immutable: true },
   prefixUrl: "https://github.com/metonym/svelte-gestalt-icons/tree/master/",
+  style: `
+    .code-fence > div {
+      display: inline-flex;
+      flex-direction: column;
+      border: 1px solid #eaecef;
+      border-radius: 4px;
+      padding: 8px;
+      margin: 4px;
+      font-size: 0.75rem;
+    }
+
+    .code-fence svg {
+      margin-bottom: 4px;
+    }
+  `
 });
